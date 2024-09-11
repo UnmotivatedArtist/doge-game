a909295e70a8dd3f232a8e5afee02fb670ac177a
UnmotivatedArtist <Thomas.lewis.ross@gmail.com>
2024-09-11 11:11:14 -0500

    Fixed player movement and added mob

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400 #The speed of the player in pixles/sec (may change later so it feels right)
 var screen_size # Size of the game window
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -17,13 +20,30 @@ func _process(delta):
 		velocity.y -=1
 	if Input.is_action_pressed("move_down"):
 		velocity.y +=1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h < 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+@warning_ignore("unused_parameter")
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
